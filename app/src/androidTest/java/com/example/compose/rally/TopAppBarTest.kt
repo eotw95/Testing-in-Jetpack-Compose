ad6750c21538fedb4b508181fa41b9d1134d88dc
DESKTOP-9REHMCV\koyama ryuji <ryujikoyama95@gmail.com>
2023-08-12 09:52:23 +0900

    Add rallyTopAppBarTest_currentLabelExists

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,9 @@ package com.example.compose.rally
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
@@ -27,5 +30,23 @@ class TopAppBarTest {
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
             .assertIsSelected()
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        composeTestRule.setContent {
+            val allScreens = RallyScreen.values().toList()
+            RallyTheme {
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = {},
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
+            composeTestRule.onRoot().printToLog("currentLabelExists")
+            composeTestRule
+                .onNodeWithContentDescription(RallyScreen.Accounts.name)
+                .assertExists()
+        }
     }
 }
